6967b2d8400f8af255ef27a9b5010705f3c58d8a
przemek890 <przemek.899@wp.pl>
2024-01-04 01:48:01 +0100

    jeden-do-jednego [ jeden ostatni wynik na kalkulatorze do jednego uzytkownika ]

---
+++ backend_app/prisma/migrations/20240103222059_/migration.sql
@@ -0,0 +1,26 @@
+-- CreateTable
+CREATE TABLE "CalculatorHistory" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "history" TEXT NOT NULL,
+    "userId" INTEGER NOT NULL,
+    CONSTRAINT "CalculatorHistory_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "calculatorHistoryId" INTEGER NOT NULL DEFAULT 0
+);
+INSERT INTO "new_User" ("createdAt", "email", "id", "password") SELECT "createdAt", "email", "id", "password" FROM "User";
+DROP TABLE "User";
+ALTER TABLE "new_User" RENAME TO "User";
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "CalculatorHistory_userId_key" ON "CalculatorHistory"("userId");
